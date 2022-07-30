2d653f6c94a124a524d8d995017748a2d857da36
dga24 <garciadavid.24@gmail.com>
2022-07-30 20:24:15 +0200

    ok

--- main.go
+++ main.go
@@ -8,15 +8,10 @@ import (
 func main() {
 	str := []string{"Colorado","Utah","Wisconsin","Oregon"}
 	sort.Strings(str)
-	for _, s := range str {
-		fmt.Println(len(s)%3)
-	}
 	fmt.Println(RemainderSorting(str))
-
 }
 
 func RemainderSorting(strArr []string) []string {
-
 	sort.SliceStable(strArr, func(i, j int) bool {
 		return len(strArr[i])%3 < len(strArr[j])%3
 	})
